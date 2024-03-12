ba0affd2fb5445f8738bc666c02edee7908dfb70
Jonathan Parris <jparris@ddn.com>
2024-03-12 16:19:55 -0600

    16   2024-03-12 16:19:53   (current)

--- nodes/ddn/flake.nix
+++ nodes/ddn/flake.nix
@@ -73,8 +73,12 @@
         initdbArgs = ["-U jparris" "--pgdata=/var/lib/postgresql/15" "--auth=trust" "--no-locale" "--encoding=UTF8"];
         package = pkgs.postgresql;
         authentication = pkgs.lib.mkOverride 10 ''
-          #type database  DBuser  auth-method
-          local all      all trust
+                #type database  DBuser  auth-method
+                local all      all trust
+          # ipv4
+          host  all      all     127.0.0.1/32   trust
+          # ipv6
+          host all       all     ::1/128        trust
         '';
         #initialScript = pkgs.writeText "backend-initScript" ''
         #  CREATE ROLE postgres WITH LOGIN PASSWORD 'emf' SUPERUSER;
