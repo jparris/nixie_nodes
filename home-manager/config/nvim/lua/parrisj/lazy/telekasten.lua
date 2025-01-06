83a888771596f5f38049023dfe66d233737dba9d
Jonathan Parris <jparris@ddn.com>
2025-01-06 16:48:39 -0700

    Telekasten

--- home-manager/config/nvim/lua/parrisj/lazy/telekasten.lua
+++ home-manager/config/nvim/lua/parrisj/lazy/telekasten.lua
@@ -5,6 +5,28 @@ return { -- telekasten
         "nvim-telescope/telescope.nvim",
     },
     config = function()
-        require("telekasten").setup({ home = vim.fn.expand("~/notes") })
+        require("telekasten").setup({
+            vim.keymap.set("n", "<leader>zw", "<cmd>Telekasten goto_thisweek<CR>"),
+
+            home = vim.fn.expand("~/notes/ddn/pages"),
+            dailies = vim.fn.expand("~/notes/ddn/journal"),
+            weeklies = vim.fn.expand("~/notes/ddn/journal"),
+
+            filename_space_subst = "_",
+
+            journal_auto_open = true,
+
+            dailies_create_nonexisting = true,
+            follow_creates_nonexisting = true,
+            weeklies_create_nonexisting = true,
+
+            vaults = {
+                personal = {
+                    home = vim.fn.expand("~/notes/personal/pages"),
+                    dailies = vim.fn.expand("~/notes/personal/journal"),
+                    weeklies = vim.fn.expand("~/notes/personal/journal"),
+                },
+            },
+        })
     end,
 }
