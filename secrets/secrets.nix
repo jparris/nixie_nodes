4e35d6fbb01299dbf77b27dabca72910e5817094
Jon Parris <parrisj@gmail.com>
2025-06-27 21:47:43 -0600

    Adding Acme secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,11 +2,11 @@ let
   utgard-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+SQ/ClXErepiIhEkIjf2IKJnWM2epWAj9zbBLJqlpG";
   utgard-parrisj = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYpTe1MlvhI5EOSber15XNXgnR8/aFhOIgp0Vh9jjRz parrisj@utgard";
   utgard = [utgard-host utgard-parrisj];
-  yggdrasil-host= "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsvf8t3oldCX2EKopEcwUFSAHIlQqij7vChdwghoYFF root@nixos";
+  yggdrasil-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsvf8t3oldCX2EKopEcwUFSAHIlQqij7vChdwghoYFF root@nixos";
   yggdrasil-parrisj = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJDgYzYsfx4lO0jcfzNvzISbSrct9/qTh3MERAvmO/up parrisj@nixos";
   yggdrasil = [yggdrasil-host yggdrasil-parrisj];
 in {
-  "acme.age".publicKeys = utgard;
+  "acme.age".publicKeys = utgard ++ yggdrasil;
   "cloudflare_ddns.age".publicKeys = yggdrasil;
   "miniflux.age".publicKeys = utgard;
   "nextcloud.age".publicKeys = utgard;
