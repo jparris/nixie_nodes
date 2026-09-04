baa716ead7c3f739793c303d0ccd04829e0e1041
Jonathan Parris <jparris@ddn.com>
2026-09-03 20:25:36 -0600

    Bash RC, Readline, and Zoxide

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -15,9 +15,60 @@
         enable = true;
         nix-direnv.enable = true;
       };
+
       bash = {
         enable = true;
-        shellAliases.ll = "ls -l";
+        sessionVariables = {
+          "EDITOR" = "nvim";
+          "PATH" = "$PATH:$HOME/.cargo/bin/";
+        };
+        bashrcExtra = ''
+          #
+          # Directory Navigation
+          #
+          shopt -s autocd         # Prepend cd to directory names automatically
+          shopt -s cdable_vars    # Use Env Vars as bookmarks - you cd into a env var from any directory
+          shopt -s cdspell        # Correct spelling errors in arguments supplied to cd
+          shopt -s dirspell       # Correct spelling errors during tab-completion
+
+          #
+          # Globing - aka filename matching
+          #
+          #shopt -s dotglob        # Matchs hidden files
+          shopt -s extglob        # Brings the power of regular expressions to globing
+          #shopt -s failglob       # Report errors -- Breaks nix-shell
+          shopt -s globstar       # Turn on recursive globbing (enables ** to recurse all directories)
+          shopt -s nocaseglob     # Case-insensitive globbing
+
+          # Highlights man pages
+          man() {
+              env \
+              LESS_TERMCAP_md=$'\e[1;36m' \
+              LESS_TERMCAP_me=$'\e[0m' \
+              LESS_TERMCAP_se=$'\e[0m' \
+              LESS_TERMCAP_so=$'\e[1;40;92m' \
+              LESS_TERMCAP_ue=$'\e[0m' \
+              LESS_TERMCAP_us=$'\e[1;32m' \
+                  man "$@"
+          }
+
+          # Borrowed from http://www.bashoneliners.com/oneliners/oneliner/231/
+          up(){
+              case $# in
+                  0 )
+                  DEEP=1 ;;
+                  1 )
+                  DEEP=$1 ;;
+              esac
+              for i in $(seq 1 $DEEP); do
+                  cd ../;
+              done;
+          }
+
+          # Disable the system bell
+          set bell-style none
+        '';
+        shellAliases.gg = "git grep";
       };
 
       git = {
@@ -38,6 +89,20 @@
         enable = true;
       };
 
+      neovim = {
+        enable = true;
+      };
+
+      readline = {
+        enable = true;
+        bindings = {
+          "\e[A" = "history-search-backward";
+          "\e[B" = "history-search-forward";
+          "\e[C" = "forward-char";
+          "\e[D" = "backward-char";
+        };
+      };
+
       starship = {
         enable = true;
         #extraPackages = [pkgs.starship-jj]
@@ -51,6 +116,7 @@
           hostname.style = "[$ssh_symbol$hostname]($style):";
         };
       };
+
       wezterm = {
         enable = true;
         extraConfig = ''
@@ -63,7 +129,13 @@
           }
         '';
       };
+
+      zoxide = {
+        enable = true;
+        enableBashIntegration = true;
+      };
     };
+
     home.stateVersion = "26.05";
   };
 }
